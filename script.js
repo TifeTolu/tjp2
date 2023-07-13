38b96842c0316330d8aaeb30bf83da3137100a06
tifetolu <tifetolulope@gmail.com>
2023-07-13 11:11:52 +0000

    fixed header

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const hamburger = document.querySelector('.hamburger')
 const nav = document.querySelector('.nav2')
 const close = document.querySelector('.close')
+const header = document.querySelector('.header')
 
 hamburger.addEventListener('click', () => {
     nav.classList.toggle('visible')
     close.classList.toggle('visible')
     hamburger.classList.toggle('visible')
+    header.classList.toggle('visible')
 })
 
 close.addEventListener('click', () => {
     nav.classList.toggle('visible')
     close.classList.toggle('visible')
     hamburger.classList.toggle('visible')
+    header.classList.toggle('visible')
 })
